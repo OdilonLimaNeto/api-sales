ad82a09cde506a212a0d57ed268148be0b47d0f5
OdilonLimaNeto <odilongeronimoo@gmail.com>
2021-11-01 00:10:33 -0500

    ✨ create new feature error handling

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,27 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { routes } from './routes';
+import { AppError } from '@shared/errors/AppError';
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
 server.use(routes);
+
+server.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response
+      .status(500)
+      .json({ status: 'error', message: 'Internal server error' });
+  },
+);
 
 server.listen(3000, () => {
   console.log('server started on port 3000');
