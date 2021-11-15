035f389f8cf2014484e3d60b7d371bf8d49a4366
OdilonLimaNeto <odilongeronimoo@gmail.com>
2021-11-15 00:22:08 -0500

    ♻️ inser new options on server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,28 +1,32 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import { routes } from './routes';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import { router } from './routes';
 import { AppError } from '@shared/errors/AppError';
-import '@shared/infra/database/typeorm';
 
+import '../container';
+import { createConnection } from 'typeorm';
+
+createConnection();
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
-server.use(routes);
+server.use(router);
 
 server.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
       });
     }
-    return response
-      .status(500)
-      .json({ status: 'error', message: 'Internal server error' });
+    return response.status(500).json({
+      status: 'Error',
+      message: `${err.message}`,
+    });
   },
 );
 
