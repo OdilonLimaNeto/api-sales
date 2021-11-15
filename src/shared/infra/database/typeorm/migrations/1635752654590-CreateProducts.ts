f6bab3a788707a0cdb8e26e8266eaeeaa7683e42
OdilonLimaNeto <odilongeronimoo@gmail.com>
2021-11-15 00:22:17 -0500

    🔥 remove file

--- src/shared/infra/database/typeorm/migrations/1635752654590-CreateProducts.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class CreateProducts1635752654590 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
