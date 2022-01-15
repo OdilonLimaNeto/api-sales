c19351c16c8f86cc5e83539d0da8ecfd2775301f
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-01-15 16:30:00 -0500

    ✨ create new route

--- src/shared/http/routes/product/product.routes.ts
+++ src/shared/http/routes/product/product.routes.ts
@@ -1,12 +1,14 @@
 import { CreateProductController } from '@modules/product/useCases/CreateProduct/CreateProductController';
+import { ListProductsController } from '@modules/product/useCases/ListProducts/ListProductsController';
 import { Router } from 'express';
 
 const products = Router();
 
 const createProductController = new CreateProductController();
+const listProductsController = new ListProductsController();
 
 products.post('/', createProductController.handler);
-// products.get('/', .handler);
+products.get('/', listProductsController.handler);
 // products.get('/:id', .handler);
 // products.delete('/:id', .handler);
 // products.put('/:id', .handler);
