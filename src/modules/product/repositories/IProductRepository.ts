abbf4fc0c7160e4d6bfd08ca3d8337e4cb263ebe
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-01-15 16:28:15 -0500

    ♻️ change param to list and findById function

--- src/modules/product/repositories/IProductRepository.ts
+++ src/modules/product/repositories/IProductRepository.ts
@@ -1,10 +1,12 @@
 import { ICreateProductDTO } from '../dtos/ICreateProdutDTO';
+import { IListProductsFilterDTO } from '../dtos/IListProdutsDTO';
 import { Product } from '../infra/typeorm/entities/Product';
 
 interface IProductRepository {
   create(data: ICreateProductDTO): Promise<Product>;
-  list(): Promise<Product[]>;
-  findByTitle(title: string): Promise<Product | undefined>;
+  list({ title, price, quantity }: IListProductsFilterDTO): Promise<Product[]>;
+  findByTitle(title: Product['title']): Promise<Product | undefined>;
+  findById(id: Product['id']): Promise<Product | undefined>;
 }
 
 export { IProductRepository };
