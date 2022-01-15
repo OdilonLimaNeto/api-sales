90c9f09bb44203d97f2f3f3bf595b3246b274285
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-01-15 16:28:59 -0500

    ♻️ refactor code in controller to list products

--- src/modules/product/useCases/ListProducts/ListProductsController.ts
+++ src/modules/product/useCases/ListProducts/ListProductsController.ts
@@ -4,9 +4,14 @@ import { ListProductsUseCase } from './ListProductsUseCase';
 
 class ListProductsController {
   async handler(request: Request, response: Response): Promise<Response> {
+    const { title, price, quantity } = request.query;
     const listProductsUseCase = container.resolve(ListProductsUseCase);
-    await listProductsUseCase.execute();
-    return response.status(201).json();
+    const products = await listProductsUseCase.execute({
+      title: title as string | undefined,
+      price: price as number | undefined,
+      quantity: quantity as number | undefined,
+    });
+    return response.status(201).json(products);
   }
 }
 
